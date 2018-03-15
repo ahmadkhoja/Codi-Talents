df0000ea06d4855f6fd444b88afc01ae36cf2087
BayanMawed <bmaweed@gmail.com>
2018-03-15 12:15:17 +0200

    Change in style

--- src/StudentTalent.js
+++ src/StudentTalent.js
@@ -17,16 +17,16 @@ const create_student = (name, availability, skills, favorite,likesteamwork,city,
 
 
 const students = [
-  create_student('Adam', '3', 'PHP', 'Reading', 'yes'     ,'beirut'),
-  create_student('mostafa', '1', 'PHP', 'Reading', 'yes'     ,'beirut'),
-  create_student('ninja', '2', 'PHP', 'Reading', 'yes'     ,'saida'),
-  create_student('pirate', '3', 'PHP', 'Reading', 'yes'     ,'jbeil'),
+  create_student('Adam', '3 months', 'PHP', 'Reading', 'yes'     ,'beirut'),
+  create_student('mostafa', '1 month', 'PHP', 'Reading', 'yes'     ,'beirut'),
+  create_student('ninja', '2 months', 'PHP', 'Reading', 'yes'     ,'saida'),
+  create_student('pirate', '3 months', 'PHP', 'Reading', 'yes'     ,'jbeil'),
   create_student('mimi', 'available', 'PHP', 'Reading', 'yes'     ,'beirut'),
-  create_student('Ahmad', '2', 'REACT', 'Sport', 'yes'  ,'saida'),
-  create_student('Amjad', 'available', 'HTML', 'Swimming', 'yes','tripoli'),
-  create_student('Hala', 'available', 'javascript', 'Reading', 'yes'    ,'jounyeh'),
-  create_student('Rami', 'available', 'WORDPRESS', 'Sport', 'yes'   ,'jbeil'),
-  create_student('Rima', '1', 'LARAVEL', 'Walking', 'yes'  ,'tyre')
+  create_student('Ahmad', '2 months', 'REACT', 'Sport', 'yes'  ,'saida'),
+  create_student('Amjad', 'Available', 'HTML', 'Swimming', 'yes','tripoli'),
+  create_student('Hala', 'Available', 'javascript', 'Reading', 'yes'    ,'jounyeh'),
+  create_student('Rami', 'Available', 'WORDPRESS', 'Sport', 'yes'   ,'jbeil'),
+  create_student('Rima', '1 month', 'LARAVEL', 'Walking', 'yes'  ,'tyre')
 ]
 const Student = ({name,availability,skills,favorite,likesteamwork,image,city}) => (
   <Col xs={1} md={4}>
@@ -36,7 +36,7 @@ const Student = ({name,availability,skills,favorite,likesteamwork,image,city}) =
       <h2>{name}</h2>
       <h3>City: {city}</h3>
       <h3>Availability: </h3>
-      <p>{availability} months</p>
+      <p>{availability}</p>
       <h3>Skills: </h3>
       <p>{skills}</p>
       <hr />
@@ -109,21 +109,21 @@ setSkillFilter = (skillFilter) => {
   <Row className="clearfix">
     <Col sm={4}>
       <Nav bsStyle="pills" stacked>
-        <NavItem eventKey="first">Places</NavItem>
-        <NavItem eventKey="second">Skills</NavItem>
-        <NavItem eventKey="third">Availability</NavItem>
+        <NavItem eventKey="first"><p><font size="5" color="Black">City</font></p></NavItem>
+        <NavItem eventKey="second"><p><font size="5" color="black">Skill</font></p></NavItem>
+        <NavItem eventKey="third"><p><font size="5" color="black">Availability</font></p></NavItem>
       </Nav>
     </Col>
     <Col sm={8}>
       <Tab.Content animation>
         <Tab.Pane eventKey="first">
             <ButtonGroup vertical block>
-                <Button onClick={() => this.setCityFilter("beirut")}>Beirut</Button>
-                <Button onClick={() => this.setCityFilter("saida")}>Saida</Button>
-                <Button onClick={() => this.setCityFilter("tripoli")}>Tripoli</Button>
-                <Button onClick={() => this.setCityFilter("jounyeh")}>Jounyeh</Button>
-                <Button onClick={() => this.setCityFilter("jbeil")}>Jbiel</Button>
-                <Button onClick={() => this.setCityFilter("tyre")}>Tyre</Button>
+                <Button onClick={() => this.setCityFilter("beirut")}><h4>Beirut</h4></Button>
+                <Button onClick={() => this.setCityFilter("saida")}><h4>Saida</h4></Button>
+                <Button onClick={() => this.setCityFilter("tripoli")}><h4>Tripoli</h4></Button>
+                <Button onClick={() => this.setCityFilter("jounyeh")}><h4>Jounyeh</h4></Button>
+                <Button onClick={() => this.setCityFilter("jbeil")}><h4>Jbiel</h4></Button>
+                <Button onClick={() => this.setCityFilter("tyre")}><h4>Tyre</h4></Button>
             </ButtonGroup>
         </Tab.Pane>
         <Tab.Pane eventKey="second">
